974d8b637f207d4f7265c6eecb9e360f8b43a183
mumbuamutuku <mumbuamutuku@gmail.com>
2023-02-28 14:26:40 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,18 +7,22 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t ldepth, rdepth;
+        size_t ldepth = 0, rdepth = 0;
 
         if (!tree)
             return (0);
         else
         {
-            ldepth = binary_tree_height(tree->left);
-            rdepth = binary_tree_height(tree->right);
-
-            if (ldepth >= rdepth)
-                return (ldepth);
-            else     
-                return (rdepth);
+            /**ldepth = binary_tree_height(tree->left);
+            * rdepth = binary_tree_height(tree->right);
+            * 
+            *if (ldepth >= rdepth)
+            *    return (ldepth);
+            *else    
+             *   return (rdepth);
+             */
+            ldepth = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+            rdepth = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+            return ((ldepth > rdepth) ? ldepth : rdepth);
         }
 }
