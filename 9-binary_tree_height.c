1352491f40bb73ecfb5c9b4506c6e689cf367630
mumbuamutuku <mumbuamutuku@gmail.com>
2023-02-28 14:10:11 +0300

    8 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t ldepth = 0, rdepth = 0;
+        size_t ldepth, rdepth;
 
         if (tree == NULL)
             return (0);
