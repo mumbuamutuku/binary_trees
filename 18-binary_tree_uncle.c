cebdcd329e4518099afa2bdc0b99e1e6daa57ed9
mumbuamutuku <mumbuamutuku@gmail.com>
2023-03-02 01:06:30 -0800

    18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/**
+ * binary_tree_uncle - Finds the sibling of a node
+ * @node: The node to find the sibling of
+ *
+ * Return: Either NULL or the sibling node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node)
+		return (binary_tree_sibling(node->parent));
+	return (NULL);
+}
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: The node to find the sibling of
+ *
+ * Return: Either NULL or the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *fam = NULL;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	fam = node->parent;
+
+	if (fam->left == node)
+		fam = fam->right;
+	else
+		fam = fam->left;
+
+	if (fam)
+		return (fam);
+
+	return (NULL);
+}
